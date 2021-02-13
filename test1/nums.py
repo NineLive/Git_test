deff148d5183930d01992eb417904b5b6031e9d8
NineLive <chesnokov_sergey9@mail.ru>
2021-02-13 19:42:44 +0300

    Create module strings.py Delete text from nums.py

--- test1/nums.py
+++ test1/nums.py
@@ -1,5 +1,14 @@
 def plus(a, b):
     return a + b
 
+
+
+def minus(a, b):
+    return a - b
+
+def multy(a, b):
+    return a * b
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(6, 4))
+    print(multy(7, 8))
